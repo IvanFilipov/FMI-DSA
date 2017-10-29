c797c8f9aa1007bbcf6cbfbae324dd769c475cb2
IvanFilipov <vanaka1189@gmail.com>
2017-10-30 00:44:42 +0200

    static stack basic C - style implementation

---
+++ DataStructures/StaticStack/StaticStack.c
@@ -0,0 +1,64 @@
+//the simplest implementation of static stack
+//written in C, so could be compile using gcc
+
+#include<stdio.h>
+
+
+#define MAXN 10
+#define INPUTN 7 
+
+typedef int data;
+
+data stack[MAXN];
+int top;
+
+void Init() {
+
+	top = 0;
+}
+
+int isEmpty() {
+
+	return top == 0;
+}
+
+void push(data elem) {
+
+	if (top == MAXN)
+		fprintf(stderr, "not enough space in the stack!");
+	else
+		stack[top++] = elem;
+
+}
+
+data pop() {
+
+	if (isEmpty()) {
+
+		fprintf(stderr, "empty stack!");
+		return 0;
+	}
+
+	return stack[--top];
+
+}
+
+
+int main() {
+
+	int cnt = INPUTN;
+	data p;
+	Init();
+
+	do {
+		scanf("%d", &p);
+		push(p);
+
+	} while (--cnt);
+
+	while (!isEmpty())
+		printf("%d", pop());
+
+	printf("\n");
+	return 0;
+}
