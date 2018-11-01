267dea792f0cafa7bbfc56994c1d28bdad7adc11
IvanFilipov <vanaka1189@gmail.com>
2018-11-02 00:40:05 +0200

    boosting the example about dynamic array
    removing :
    - the inner logic about "is sorted"
    adding :
    - constructor with initalizer list
    - random access iterator ( STL friendly )
    - advanced tests ( using STL algorithms, modern loops )
    - much more comments

--- data_structures/dynamic_array/dynamic_array.hpp
+++ data_structures/dynamic_array/dynamic_array.hpp
@@ -1,107 +1,246 @@
-#include <utility> // std::swap
-#include <stdexcept> //standard exceptions
-#include <iosfwd> //forward declarations of ostreams
+/*
+ * The basic linear data structure - dynamic array. With random access iterator. STL friendly.
+ * This file is part of the "Data structures and algorithms" course. FMI 2018/19
+ *
+ * Author : Ivan Filipov	
+ */
 
-template<typename T>
+#include <utility>   // std::swap
+#include <stdexcept> // standard exceptions
+#include <iosfwd>    // forward declarations of ostreams
+#include <initializer_list> // initializer_list
+
+namespace dsa { // the "course" name space
+template<typename T> // the data type
 class dynamic_array {
 
 private :
-
-	T* data_ptr;
-	size_t cur_size;
-	size_t capacity;
-
-	bool is_sorted;
+	/* private data members */
+	T* data_ptr;     // the data chunk
+	size_t cur_size; // actual number of elements
+	size_t capacity; // maximum current size, if we reach this point, reallocation in necessary	
 
 public:
+	/* object life cycle */
+	dynamic_array(); // default constructor
+	dynamic_array(size_t size); // constructor with parameter size, allocates memory for "size" objects
+	dynamic_array(std::initializer_list<T> ilist); // constructor with initializer list { ... }
+	dynamic_array(const dynamic_array& rhs); // copy constructor
+	dynamic_array& operator=(const dynamic_array& rhs); // assignment operator
+	~dynamic_array(); // destructor
+	
+public :
+	/* iterator */
+	// very basic random access iterator
+	// a better implementation will give a const_iterator, too
+	class iterator {
+		
+	private :
 
-	//THE BIG FOUR
-	dynamic_array();
-	~dynamic_array();
-	dynamic_array(const dynamic_array& rhs);
-	dynamic_array& operator=(const dynamic_array& rhs);
+		T* elem_ptr;
 
-	//helpers
-private:
+	public :
+		// lets make our iterator more STL friendly 
+		// see more at : https://en.cppreference.com/w/cpp/iterator/iterator_traits
+		typedef iterator                  self_type;
+		typedef T                         value_type;
+		typedef T&                        reference;
+		typedef T*                        pointer;
+		typedef std::random_access_iterator_tag
+		                                  iterator_category;
+		typedef int                       difference_type;
+		// basic constructor
+		iterator(pointer e) : elem_ptr(e) { }
+		
+	public :
+		
+		// access operators
+		reference operator*() { return *elem_ptr; }
+		pointer operator->() { return elem_ptr; }
+		reference operator[](size_t i) { return  *(elem_ptr + i); }
+		
+		// compare operators
+		bool operator==(const self_type& rhs) const { return elem_ptr == rhs.elem_ptr; }
+		bool operator!=(const self_type& rhs) const { return !(rhs == *this); }
+		bool operator<(const self_type& rhs) const { return elem_ptr < rhs.elem_ptr; } // compare the addresses
+		bool operator>(const self_type& rhs) const { return (rhs < *this); }
+		bool operator<=(const self_type& rhs) const { return !(*this > rhs); }
+		bool operator>=(const self_type& rhs) const { return !(*this < rhs); }
+		
+		// modifier operators
+		self_type& operator++() {
+			
+			++elem_ptr;
+			return *this;
+		}
 
+		self_type operator++(int) {
+
+			self_type res(*this);
+			++(*this);
+			return res;
+		}
+		
+		self_type& operator--() {
+			
+			--elem_ptr;
+			return *this;
+		}
+
+		self_type operator--(int) {
+
+			self_type res(*this);
+			--(*this);
+			return res;
+		}
+		
+		self_type& operator+=(int i) {
+			
+			difference_type m = i;
+			if (m >= 0) while (m--) ++elem_ptr; // forward moving
+			else while (m++) --elem_ptr; // backward moving
+			return *this;
+		}
+		
+		self_type operator+(int i) const {
+			
+			self_type res(*this);
+			return res += i;
+		}
+		
+		self_type& operator-=(int i) {
+			
+			return *this+=(-i);
+		}
+		
+		self_type operator-(int i) const {
+			
+			self_type res(*this);
+			return res -= i;
+		}
+		
+		// difference between two iterators
+		difference_type operator-(const self_type& rhs) const {
+			
+			return elem_ptr - rhs.elem_ptr;
+		}
+	};
+	
+private :
+	/* helpers */
+	// copy all objects from another dynamic array
 	void copy_from(const dynamic_array& rhs);
-   
+    // reallocate internal buffer memory
 	void resize(size_t new_cap);
 	
-	//returns the index of the searched element, -1 if there is no such element
-	int binary_search(const T& el, int left, int right) const;
-	int linear_search(const T& el) const;
+	// returns an iterator with index of the searched element
+	// if the element is not found, returns iterator to end()
+	iterator binary_search(const T& el, int left, int right) const;
+	iterator linear_search(const T& el) const;
+	/** binary search has O(logN) complexity,
+		linear search - 0(N).
+	*/
 	
-public:
-
-	//frees the allocated memory
+public :
+	/* interface */
+	// free the allocated memory
 	void clear();
-	//add a new elem in the end
+	
+	// modifiers : 
+	// add a new elem in the end
 	void push_back(const T& new_el);
 	//removes the last elem
-	void pop_back();//both with amortized O(1) complexity,
-					//because memory reallocations
-
-
-	//new elem on random position
-	void insert_at(size_t index , const T& new_el);
-
-	//removes an elem on random position
-	void remove_at(size_t index);
-
+	void pop_back();
+	/** both with amortized O(1) complexity,
+		because memory reallocations.
+	*/
+	// add new element on random position
+	// return iterator to the new element
+	iterator insert_at(const iterator& index , const T& new_el);
+	// removes an elem on random position
+	// returns iterator following element of the deleted one
+	iterator remove_at(const iterator& index, bool keep_order = true);
+	/** insert_at with O(N) time complexity.
+		remove_at with O(N) default case complexity,
+		but could be optimized to O(1), if element sequence is not important.
+	*/
+	
+	// access operators :
+	// at random index
 	const T& operator[](size_t index) const;
 	T& operator[](size_t index);
-
-	size_t get_size() const;
+	// at the end
+	const T& back() const;
+	T& back();
+	// at beginning
+	const T& front() const;
+	T& front();
+	
+	// getters :
+	// returns the current number of elements
+	size_t size() const;
+	// returns the current capacity
 	size_t get_capacity() const;
+	// return true if the dynamic array is empty, false else
+	bool empty() const;
 
-	//searches for an element and returns it's index if founded  , -1 else
-	int search(const T& el) const;
+	// searches for an element and returns iterator to it if found
+	// return iterator to end, else
+	// a parameter is_sorted could be passed to improve speed
+	iterator find(const T& el, bool is_sorted = false) const;
+	
+	// iterators creation interface 
+	iterator begin() {
+		
+		return iterator(data_ptr);
+	}
+	
+	iterator end() {
+		
+		return iterator(data_ptr + cur_size);
+	}
+	
+	// constant iterator objects. they have limited usage
+	// in our implementation, much better is to create
+	// another class : const_iterator
+	const iterator cbegin() const {
+		
+		return iterator(data_ptr);
+	}
+	
+	const iterator cend() const {
+		
+		return iterator(data_ptr + cur_size);
+	}
 
-	//sorts the vector using the simple insertion sort algorithm
-	void sort();
-
-	//simple print functions
+	// simple output functions for easier debugging
 	void print_info(std::ostream& os) const;
 	void print_elems(std::ostream& os) const; 
 };
 
+/* object life cycle */
+
 template<typename T>
 dynamic_array<T>::dynamic_array() : data_ptr(nullptr), cur_size(0), capacity(0) {
-
 	/*...*/
 }
 
 template<typename T>
-dynamic_array<T>::~dynamic_array() {
-
-	clear();
+dynamic_array<T>::dynamic_array(size_t size) : 
+	data_ptr(new T[size]), cur_size(0), capacity(size) {
+	/*...*/
 }
 
 template<typename T>
-void dynamic_array<T>::clear() {
+dynamic_array<T>::dynamic_array(std::initializer_list<T> ilist) : dynamic_array(ilist.size()) {
 	
-	delete[] data_ptr;
-
-	data_ptr = nullptr;
-	cur_size = 0;
-	capacity = 0;
-}
-
-template<typename T>
-void dynamic_array<T>::copy_from(const dynamic_array<T>& rhs) {
-
-	data_ptr = new T[rhs.capacity];
-
-	//memcpy(data, other.data, sizeof(T)*other.CurSize);
-	//will not do the trick, because it will make a shallow copy of
-	//our objects
-	for (size_t i = 0; i < rhs.cur_size; i++)
-		data_ptr[i] = rhs.data_ptr[i];
-
-	cur_size = rhs.cur_size;
-	capacity = rhs.capacity;
-	is_sorted = rhs.is_sorted;
+	// notice that we have called the constructor with "size"
+	// parameter to allocate memory only once, not to resize
+	// each time new element from the initializer list is added
+	size_t i = 0;
+	for(const T& el : ilist)    // for each element from the initializer list
+		data_ptr[i++] = el;     // take its value and write it into our array
+	cur_size = (i) ? i - 1 : 0; // remember how much elements we have read
 }
 
 template<typename T>
@@ -123,10 +262,33 @@ dynamic_array<T>& dynamic_array<T>::operator=(const dynamic_array<T>& other) {
 }
 
 template<typename T>
+dynamic_array<T>::~dynamic_array() {
+
+	clear();
+}
+
+/* helpers */
+
+template<typename T>
+void dynamic_array<T>::copy_from(const dynamic_array<T>& rhs) {
+
+	data_ptr = new T[rhs.capacity];
+
+	// memcpy(data, other.data, sizeof(T) * rhs.cur_size);
+	// will not do the trick, because it will make a shallow copy of
+	// our objects
+	for (size_t i = 0; i < rhs.cur_size; i++)
+		data_ptr[i] = rhs.data_ptr[i];
+
+	cur_size = rhs.cur_size;
+	capacity = rhs.capacity;
+}
+
+template<typename T>
 void dynamic_array<T>::resize(size_t new_cap) {
 
 	T *temp = data_ptr;
-
+	
 	data_ptr = new T[new_cap];
 
 	for (size_t i = 0; i < cur_size; i++)
@@ -138,8 +300,53 @@ void dynamic_array<T>::resize(size_t new_cap) {
 }
 
 template<typename T>
-void dynamic_array<T>::push_back(const T& new_el) {
+typename dynamic_array<T>::iterator dynamic_array<T>::
+	binary_search(const T& el, int left, int right) const {
 
+	//there is no such element
+	if (left > right)
+		return cend();
+
+	int med = (left + right) / 2;
+
+	if (data_ptr[med] == el)
+		return iterator(data_ptr + med);
+
+	if (data_ptr[med] > el)
+		return binary_search(el, left, med - 1);
+
+	if(data_ptr[med] < el)
+		return binary_search(el, med + 1, right);
+		
+	//won't reach this line, but added just to stop compiler's complaining
+	return cend();
+}
+
+template<typename T>
+typename dynamic_array<T>::iterator dynamic_array<T>::linear_search(const T& el) const {
+
+	for (size_t i = 0; i < cur_size; i++)
+		if (data_ptr[i] == el)
+			return iterator(data_ptr + i);
+
+	return cend();
+}
+
+/* interface */
+
+template<typename T>
+void dynamic_array<T>::clear() {
+	
+	delete[] data_ptr;
+
+	data_ptr = nullptr;
+	cur_size = 0;
+	capacity = 0;
+}
+
+template<typename T>
+void dynamic_array<T>::push_back(const T& new_el) {
+	// is a reallocation needed
 	if (cur_size >= capacity) {
 
 		size_t new_cap = (capacity == 0 ? 2 : capacity * 2);
@@ -147,15 +354,6 @@ void dynamic_array<T>::push_back(const T& new_el) {
 	}
 
 	data_ptr[cur_size++] = new_el;
-
-	if (cur_size == 1) {
-
-		is_sorted = true;
-		return;
-	}
-
-	if(data_ptr[cur_size - 2] > data_ptr[cur_size - 1])
-		is_sorted = false;
 }
 
 template<typename T>
@@ -164,8 +362,8 @@ void dynamic_array<T>::pop_back() {
 	if (cur_size)
 		cur_size--;
 	else
-		throw std::length_error("already empty!");
-
+		throw std::logic_error("already empty dynamic array!");
+	// optional, downsizing sometimes only slows down 
 	if (cur_size * 2 <= capacity) {
 
 		size_t new_cap = ((cur_size == 0) ? 0 : capacity / 2);
@@ -174,49 +372,45 @@ void dynamic_array<T>::pop_back() {
 }
 
 template<typename T>
-void dynamic_array<T>::insert_at(size_t index, const T& new_el) {
+typename dynamic_array<T>::iterator dynamic_array<T>::
+	insert_at(const iterator& it, const T& new_el) {
 	
-	bool sorted_before = is_sorted;
-
-	push_back(new_el); //will take care of the memory 
-
-	if (index >= cur_size) //the element should be the last one
-		return;
-
-	if (sorted_before)
-		is_sorted = true; //maybe our elem will go to its sorted position
-
+	push_back(new_el); //will take care for the memory 
+	
+	size_t index = it - data_ptr; // the difference between the pointers it the wanted index
 	//now we should 'roll' our element back to the wanted position 
 	for (size_t i = cur_size - 1; i > index; i--) {
 
-		if (data_ptr[i] > data_ptr[i - 1])
-			is_sorted = false;
-
 		std::swap(data_ptr[i], data_ptr[i - 1]);
 	}
+	
+	return iterator(data_ptr + index);
 }
 
 template<typename T>
-void dynamic_array<T>::remove_at(size_t index) {
-
+typename dynamic_array<T>::iterator dynamic_array<T>::
+	remove_at(const iterator& it, bool keep_order) {
+	
+	size_t index = it - iterator(data_ptr);
 	if (index >= cur_size || cur_size == 1) { //just removes the last elem
 		
 		pop_back();
-		return;
+		return iterator(data_ptr + cur_size - 1);
 	}
 
-	if (!is_sorted) { //faster version O(1), but can be unexpected
+	if (!keep_order) { //faster version O(1)
 		
-		std::swap(data_ptr[cur_size - 1], data_ptr[index]);	 //from the class' client
+		std::swap(data_ptr[cur_size - 1], data_ptr[index]);
 		pop_back();
-		return;
+		return iterator(data_ptr + index);
 	}
 
-	//else ..
+	//else rolling forward
 	for(size_t i = index; i < cur_size - 1; i++)
 		std::swap(data_ptr[i], data_ptr[i+1]);
 
 	pop_back();
+	return iterator(data_ptr + index);
 }
 
 template<typename T>
@@ -237,10 +431,32 @@ T& dynamic_array<T>::operator[](size_t index) {
 			static_cast<const dynamic_array&>(*this)[index]
 	);
 }
+template<typename T>
+const T& dynamic_array<T>::back() const {
+	
+	return (*this)[cur_size - 1];
+}
+
+template<typename T>
+T& dynamic_array<T>::back() {
+	
+	return (*this)[cur_size - 1];
+}
+
+template<typename T>
+T& dynamic_array<T>::front()  {
+	
+	return (*this)[0];
+}
+template<typename T>
+const T& dynamic_array<T>::front() const {
+	
+	return (*this)[0];
+}
 
 template<typename T>
 inline
-size_t dynamic_array<T>::get_size() const {
+size_t dynamic_array<T>::size() const {
 
 	return cur_size;
 }
@@ -251,23 +467,14 @@ size_t dynamic_array<T>::get_capacity() const {
 
 	return capacity;
 }
-
 template<typename T>
-void dynamic_array<T>::sort() {
-
-	if (is_sorted)
-		return;
-
-	//complexity O(n^2)
-	for (size_t i = 1; i < cur_size; i++)
-		for (size_t j = i ; j > 0 && data_ptr[j] < data_ptr[j-1]; j--)
-			std::swap(data_ptr[j], data_ptr[j-1]);
-
-	is_sorted = true;
+inline
+bool dynamic_array<T>::empty() const {
+	return cur_size == 0;
 }
 
 template<typename T>
-int dynamic_array<T>::search(const T& el) const {
+typename dynamic_array<T>::iterator dynamic_array<T>::find(const T& el, bool is_sorted) const {
 
 	if (is_sorted)
 		return binary_search(el, 0, cur_size); // O(logN)
@@ -276,38 +483,7 @@ int dynamic_array<T>::search(const T& el) const {
 }
 
 template<typename T>
-int dynamic_array<T>::binary_search(const T& el, int left, int right) const {
-
-	//there is no such element
-	if (left > right)
-		return -1;
-
-	int med = (left + right) / 2;
-
-	if (data_ptr[med] == el)
-		return med;
-
-	if (data_ptr[med] > el)
-		return binary_search(el, left, med - 1);
-
-	if(data_ptr[med] < el)
-		return binary_search(el, med + 1, right);
-		
-	//won't reach this line, but added just to stop compiler's complaining
-	return -1;
-}
-
-template<typename T>
-int dynamic_array<T>::linear_search(const T& el) const {
-
-	for (size_t i = 0; i < cur_size; i++)
-		if (data_ptr[i] == el)
-			return i;
-
-	return -1;
-}
-
-template<typename T>
+inline
 void dynamic_array<T>::print_info(std::ostream& os) const {
 
 	os  << "obj at : Ox" << this
@@ -336,7 +512,7 @@ void dynamic_array<T>::print_elems(std::ostream& os) const {
 	
 	os << "{ ";
 	for(size_t i = 0; i < cur_size - 1; i++)
-		os << data_ptr[i] << ", ";
-		
+		os << data_ptr[i] << ", ";		
 	os << data_ptr[cur_size - 1] << " }" << std::endl;
 }
+} // namespace dsa
--- data_structures/dynamic_array/example.cpp
+++ data_structures/dynamic_array/example.cpp
@@ -1,34 +1,54 @@
-#include <stdlib.h> //rand, srand
-#include <time.h> //time
-#include <iostream> //std::cout
+/*
+ * Example usage of custom dynamic array. Some benchmarking and STL algorithms.
+ * This file is part of the "Data structures and algorithms" course. FMI 2018/19
+ *
+ * Author : Ivan Filipov	
+ */
+
+#include <random>    // std::random_device, std::mt19937
+#include <iostream>  // std::cout
+#include <algorithm> // std::sort(), std::shuffle(), std::is_sorted(), std::for_each()
 
 #include "dynamic_array.hpp"
-#include "../../utils/benchmark.hpp"
+#include "../../utils/benchmark.hpp" // custom benchmark function
 
-const int N_FILL = 100000;
-const int MAX_EL = 100000;
+using dsa::dynamic_array;
+using dsa::benchmark_test_fnc;
+using dsa::T_FORMAT;
 
-//an DynamicArray global object
+const int N_FILL = 1000000;
+const int MAX_EL = 1000000;
+
+// random generator modern c++ style : https://en.cppreference.com/w/cpp/numeric/random/mersenne_twister_engine
+std::random_device rd; // create random device
+std::mt19937 random_generator(rd());  // create random generator based on that device
+
+// a dynamic_array global object, used in benchmarking examples
 dynamic_array<int> arr;
 
-//a function that fills our array with 100000 random elements
-//all positive < 100000
+// a function that fills our array with N_FILL random elements
+// all positive < MAX_EL
 void init() {
 
 	for (int i = 0; i < N_FILL; i++)
-		arr.push_back(rand() % MAX_EL);
+		arr.push_back(random_generator() % MAX_EL);
 }
-
-//searching in the worst case... (no such element)
-void run_search() {
+// searching in the worst case... (no such element)
+// using linear search algorithm
+void run_search_slow() {
 	
-	arr.search(-1);
+	arr.find(-1);
 }
-
-//sort the array
+// using binary search algorithm
+void run_search_fast() {
+	
+	arr.find(-1, true);
+}
+// sort the array
 void run_sort() {
-	
-	arr.sort();
+	// yeah, we have random iterators,
+	// how sweet is that, we can use std::sort :)
+	std::sort(arr.begin(), arr.end());
 }
 
 //testing some basic functionality of our class
@@ -58,26 +78,26 @@ void run_basic_tests() {
 	
 	
 	std::cout << "element on index 3 : " << first[3] << std::endl; 
-	std::cout << "size : " << first.get_size() 
+	std::cout << "size : " << first.size() 
 			  << " cap : " << first.get_capacity() 
 			  << std::endl;
 	
-	first.insert_at(33, 25);
+	first.insert_at(first.begin() + 33, 25);
 	std::cout << "after insert value 25 at index 33 :" << std::endl;
 	first.print_elems(std::cout);
 	std::cout << std::endl;
 	
-	first.insert_at(4, 14);
+	first.insert_at(first.begin() + 4, 14);
 	std::cout << "after insert value 14 at index 4 :" << std::endl;
 	first.print_elems(std::cout);
 	std::cout << std::endl;
 	
-	first.remove_at(58);
+	first.remove_at(first.begin() + 58);
 	std::cout << "after removing at index 58 :" << std::endl;
 	first.print_elems(std::cout);
 	std::cout << std::endl;
 	
-	first.remove_at(4);
+	first.remove_at(first.begin() + 4);
 	std::cout << "after removing at index 4 :" << std::endl;
 	first.print_elems(std::cout);
 	std::cout << std::endl;
@@ -88,25 +108,79 @@ void run_basic_tests() {
 	second.print_elems(std::cout);
 }
 
+void run_advanced_tests() {
+	
+	std::cout << std::endl 
+	          << "---------------------------"
+			  << std::endl
+	          << "modern C++ tests"
+			  << std::endl << std::endl;
+	/* 
+	 because we have iterator, which is STL compatible
+	 we can take advantage of : 
+	 moder C++ look-like iterations
+	 STL algorithms, which take random access iterators
+	*/
+	
+	// create a test array with initializer list
+	dynamic_array<int> f_arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+	
+	std::cout << "iterating over all elements using iterator based for :" << std::endl;
+	for (dynamic_array<int>::iterator it = f_arr.begin(); it != f_arr.end(); ++it) {
+		std::cout << *it << " ";
+	}
+	std::cout << std::endl << std::endl;
+	
+	std::cout << "iterating over all elements using range based for :" << std::endl;
+	for (int el : f_arr) {
+		std::cout << el << " ";
+	}
+	std::cout << std::endl << std::endl;
+	
+	std::cout << "\nTESTING SOME STL ALGORITHM FUNCTIONS :" << std::endl;
+	std::cout << "\nelements after a shuffle :" << std::endl;
+	
+	std::shuffle(f_arr.begin(), f_arr.end(), random_generator);
+	f_arr.print_elems(std::cout);
+	std::cout << std::endl;
+	
+	
+	int num_items3 = std::count_if(f_arr.begin(), f_arr.end(), [](int i) -> bool { return i % 3 == 0; });
+    std::cout << "numbers divisible by three: " << num_items3 << std::endl;
+	
+	std::cout << "\nour elements multiplied by three :" << std::endl;
+	std::for_each(f_arr.begin(), f_arr.end(), [](int& i) -> void { i *= 3; });
+	f_arr.print_elems(std::cout);
+
+	std::cout << "\nis the array sorted? :" 
+			  << ((std::is_sorted(f_arr.begin(), f_arr.end())) ? "yes" : "no") 
+			  << std::endl;
+	
+	std::cout << "elements after sorting them :" << std::endl;
+	std::sort(f_arr.begin(), f_arr.end());
+	f_arr.print_elems(std::cout);
+	std::cout << std::endl;
+}
+
 int main() {
 
-	/* seed the random generator */
-	srand(static_cast<unsigned int>(time(nullptr)));
-	
-	/* initialize the array */
+	/* initialize the global array */
 	init();
 
 	/* test 1 linear searching */
-	benchmark_test_fnc(run_search, T_FORMAT::F_MICRO, "linear searching");
+	benchmark_test_fnc(run_search_slow, T_FORMAT::F_MICRO, "linear searching");
 
 	/* sort the array */
-	benchmark_test_fnc(run_sort, T_FORMAT::F_ALL, "slow sorting");
+	benchmark_test_fnc(run_sort, T_FORMAT::F_ALL, "sorting");
 
 	/* test 2 binary searching */
-	benchmark_test_fnc(run_search, T_FORMAT::F_MICRO, "binary searching");
+	benchmark_test_fnc(run_search_fast, T_FORMAT::F_MICRO, "binary searching");
 	
 	/* test 3 basic functionality */
 	run_basic_tests();
+	
+	/* test 4 advanced functionality */
+	run_advanced_tests();
 	
 	return 0;
 }
