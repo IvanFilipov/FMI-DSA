13ffc29ac40d9f770631f405cb210a758e7b9c9a
IvanFilipov <vanaka1189@gmail.com>
2018-11-02 00:56:09 +0200

    template queue - has two arguments T and Container
    - helps to undersand that the queue is only an adapter for array or list
    - pretty interesting benchmarking tests ( an competition between our
    implementations and the STL's ones )

---
+++ data_structures/queue/template_queue/example.cpp
@@ -0,0 +1,119 @@
+/*
+ * Example usage of the template queue idea. Some benchmarking, a competition between our data structures vs. STL.
+ * This file is part of the "Data structures and algorithms" course. FMI 2018/19
+ *
+ * Author : Ivan Filipov	
+ */
+
+#include <iostream> // std::cout
+#include <vector>   // std::vector
+#include <list>     // std::list
+#include <queue>    // std::queue
+
+#include "../../../utils/benchmark.hpp"                          // our test benchmark lib
+#include "../../dynamic_array/dynamic_array.hpp"                 // our custom vector
+#include "../../linked_list/singly_linked_list/slinked_list.hpp" // our custom singly linked list
+#include "../dynamic_queue/dynamic_queue.hpp"                    // our custom dynamic stack
+
+#include "template_queue.hpp" // the template for stack interface with variadic down-laying container 
+
+// bring here some names from our lib
+using dsa::dynamic_array;
+using dsa::t_queue;
+using dsa::benchmark_test_fnc;
+using dsa::dynamic_queue;
+using dsa::T_FORMAT;
+using dsa::slinked_list;
+
+// and also from std
+using std::vector;
+using std::list;
+using std::queue;
+
+const size_t ECOUNT = 10000;
+
+template<typename Container>
+void test_queue_adaptor() {
+	
+	t_queue<int, Container> q;
+	
+	// adding elements
+	for(size_t i = 0; i < ECOUNT; i++)
+		q.push(static_cast<int>(i));
+
+	// make a copy
+	t_queue<int, Container> q2 =q;
+	q2.push(11); // just to be sure that stack2 is used, 
+		             // because the compiler could optimize the code
+	
+	// check is empty?
+	std::cout << "\nempty " << (q.empty() ? "yes" : "no");
+	std::cout << " | size " << q.size();
+	
+	unsigned long long sum = 0;
+	// remove elements
+	while(!q.empty()) {
+		
+		sum += q.front();
+		q.pop();
+	}
+	
+	std::cout << " | sum = " << sum << std::endl;
+	
+	//destructor for st and st2
+}
+
+template<typename Queue>
+void test_queue_plain() {
+	
+	Queue q;
+	
+	// adding elements
+	for(size_t i = 0; i < ECOUNT; i++)
+		q.push(static_cast<int>(i));
+
+	// make a copy
+	Queue q2 = q;
+	q2.push(11); // just to be sure that stack2 is used, 
+		             // because the compiler could optimize the code
+	
+	// check is empty?
+	std::cout << "\nempty " << (q.empty() ? "yes" : "no");
+	std::cout << " | size " << q.size();
+	
+	unsigned long long sum = 0;
+	// remove elements
+	while(!q.empty()) {
+		
+		sum += q.front();
+		q.pop();
+	}
+	
+	std::cout << " | sum = " << sum << std::endl;
+	//destructor for st and st2
+}
+
+int main() {
+	
+	/* test operations with std::vector */
+	benchmark_test_fnc(test_queue_adaptor<vector<int>>, T_FORMAT::F_MILLI, "implementation on std::vector");
+	
+	/* test operations with our custom vector */
+	benchmark_test_fnc(test_queue_adaptor<dynamic_array<int>>, T_FORMAT::F_MILLI, "implementation on dsa::dynamic_array");
+	
+	/* test operations with std::list */
+	benchmark_test_fnc(test_queue_adaptor<list<int>>, T_FORMAT::F_MILLI, "implementation on std::list");
+	
+	/* test operations with our custom singly linked list */
+	/// !!! here the situation in the competition between our list and std::list is different, because
+	/// the queue only needs pop_front & push_back, both O(1) in these implementations, but std::list is doubly linked. 
+	benchmark_test_fnc(test_queue_adaptor<slinked_list<int>>, T_FORMAT::F_MILLI, "implementation on dsa::slinked_list");
+	
+	/* test operations with std::queue */
+	benchmark_test_fnc(test_queue_plain<queue<int>>, T_FORMAT::F_MILLI, "implementation on std::queue");
+	
+	/* test operations with our custom dynamic queue */
+	benchmark_test_fnc(test_queue_plain<dynamic_queue<int>>, T_FORMAT::F_MILLI, "implementation on dsa::dynamic_queue");
+		
+	// TO DO : add test std::list vs. dsa::doubly_linked_list
+}
---
+++ data_structures/queue/template_queue/template_queue.hpp
@@ -0,0 +1,54 @@
+/*
+ * A template generalizing the idea that the queue is only an adapter for linked list or array.
+ * This file is part of the "Data structures and algorithms" course. FMI 2018/19
+ *
+ * Author : Ivan Filipov	
+ */
+#pragma once 
+
+namespace dsa {
+template<typename T, typename Container>
+class t_queue {
+	
+private:
+	// the laying down container could be different, 
+	// it only should have an interface : back, push/pop_back, empty, size
+	Container container;
+
+public:
+	// interface :
+	
+	// letting us to "see" the top or to "adjust" it
+	T& front() { return container.front(); }
+	const T& front() const { return container.front(); }
+	
+	T& back() { return container.back(); }
+	const T& back() const { return container.back(); }
+
+	// remove / add top element
+	void push(const T& el) { container.push_back(el); }
+	void pop() { container.pop_front(); }
+	
+	// getters
+	bool empty() const { return container.empty(); }
+	size_t size() const { return container.size(); }
+};
+
+// specialization for std::vector<int>, because there is no
+// pop_front in vector's interface
+template<>
+void t_queue<int, std::vector<int>>::pop() {
+	
+	std::swap(container[0], container[container.size() - 1]);
+	container.pop_back();
+}
+
+// specialization for dsa::dynamic_array<int>
+template<>
+void t_queue<int, dsa::dynamic_array<int>>::pop() {
+	
+	std::swap(container[0], container[container.size() - 1]);
+	container.pop_back();
+}
+
+} // namespace dsa
