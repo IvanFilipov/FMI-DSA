47eac3ade0834247eb5674cd756dd4911db11b0f
IvanFilipov <vanaka1189@gmail.com>
2018-11-23 01:42:05 +0200

    example about priority queue : code refactoring
    - code style stick to repo's convention
    - use dsa::dynamic_array instead of custom dynamic array
    - a bug in siftDown fixed ( < was used, not comp function call )
    - Compare -> BinaryPredicate
    - adding more tests

---
+++ data_structures/queue/priority_queue/example.cpp
@@ -0,0 +1,119 @@
+/*
+ * Example usage of custom priority queue.
+ * This file is part of the "Data structures and algorithms" course. FMI 2018/19
+ *
+ * Author    : Gratsiela Gancheva
+ * Co-author : Ivan Filipov	
+ */
+
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+#include "priority_queue.hpp"
+#include "../../../utils/student.h"
+
+void run_basic_tests() {
+	// Create queue
+	dsa::priority_queue<int> pqueue;
+	std::cout << "Successful created priority queue!\n\n";
+	// Test whether container is empty
+	std::cout << "Is empty: " << ((pqueue.empty()) ? "True\n\n" : "False\n\n");
+	// Get size
+	std::cout << "Size: " << pqueue.size() << "\n\n";
+	// Get top element
+	try {
+		std::cout << "Get top element: ";
+		std::cout << pqueue.top() << std::endl;
+	} catch (const std::out_of_range& e) {
+		std::cout << e.what()<<std::endl;
+	}
+	std::cout << std::endl;
+	 // Insert elements
+	srand(time(NULL));
+	for (size_t i = 0; i < 10; i++) {
+		int el = rand() % 10;
+		pqueue.push(el);
+		std::cout << "Successful added element: " << el << std::endl;
+	}
+	std::cout << std::endl;
+	// Remove elements
+	while (!pqueue.empty()) {
+		std::cout << "See and pop the top element... " << pqueue.top() << std::endl;
+		pqueue.pop();
+	}
+	std::cout << std::endl;
+	// Insert new element
+	pqueue.push(100);
+	std::cout << "Successful added element: " << 100 << "\n\n";
+	// Clear queue
+	std::cout << "Size before clear: " << pqueue.size() << std::endl;
+	pqueue.clear();
+	std::cout << "Size after clear: " << pqueue.size() << std::endl;
+	std::cout << "------------------------" << std::endl;
+}
+
+template<typename PQueue>
+void feed_with_some_students(PQueue& q) {
+	
+	q.push({ "Ivancho", 40000 });
+	q.push({ "Mariika", 25000 });
+	q.push({ "Gencho", 50001 });
+	q.push({ "Pencho", 25000 });
+	q.push({ "Genka", 42000 });
+	q.push({ "Penka", 25000 });
+	q.push({ "Kalin", 40000 });
+	q.push({ "Kalinka", 25000 });
+}
+
+void run_advanced_tests() {
+	
+	// Create queue with students and custom comparator
+	// passed as lambda function
+	auto cmp_students_fn = [](const dsa::student& lhs, const dsa::student& rhs) { return lhs.fn < rhs.fn; };
+	dsa::priority_queue <dsa::student,
+						 decltype(cmp_students_fn)           
+						> pqueue (cmp_students_fn);
+						
+	feed_with_some_students(pqueue);
+	std::cout << "Students from queue ordered by fn :" << std::endl;
+	int i = 0;
+	while (!pqueue.empty()) {
+		std::cout << "# " << i++ << pqueue.top() << std::endl;
+		pqueue.pop();
+	}
+	std::cout << std::endl << std::endl;
+	
+	dsa::priority_queue <dsa::student> pnqueue;
+						
+	feed_with_some_students(pnqueue);
+	std::cout << "Students from queue ordered by name (default operator<) :" << std::endl;
+	i = 0;
+	while (!pnqueue.empty()) {
+		std::cout << "# " << i++ << pnqueue.top() << std::endl;
+		pnqueue.pop();
+	}
+	std::cout << std::endl << std::endl;
+	
+	dsa::priority_queue <dsa::student, std::greater<dsa::student>> squeue;
+						
+	feed_with_some_students(squeue);
+	std::cout << "Students from queue ordered by name (using operator>) :" << std::endl;
+	i = 0;
+	while (!squeue.empty()) {
+		std::cout << "# " << i++ << squeue.top() << std::endl;
+		squeue.pop();
+	}
+	std::cout << std::endl << std::endl;
+}
+
+int main() {
+	
+	/* test basic functionality */
+	run_basic_tests();
+	
+	/* test advanced functionality */
+	run_advanced_tests();
+	
+	return 0;
+}
---
+++ data_structures/queue/priority_queue/priority_queue.hpp
@@ -0,0 +1,174 @@
+/*
+ * An advanced linear data structure - priority queue. Using heap on array.
+ * This file is part of the "Data structures and algorithms" course. FMI 2018/19
+ *
+ * Author    : Gratsiela Gancheva
+ * Co-author : Ivan Filipov	
+ */
+ 
+#pragma once
+
+#include <utility>    // std::swap
+#include <functional> // std::less
+#include "../../dynamic_array/dynamic_array.hpp" // dsa::dynamic_array
+
+using dsa::dynamic_array;
+
+// first template argument is the type of stored objects 
+// the second one is a function to be used while comparing elements
+namespace dsa {
+template<typename T, typename Compare = std::less<T>> // use std::less as a default compare function
+class priority_queue {
+	
+private :
+	/* private data members */
+	dynamic_array<T> data; // heap on array 
+	Compare          cmp; // compare function
+	
+public :	
+	/* object life cycle */
+	// no dynamic memory to handle, so no need for big 4
+	priority_queue() = default;
+	priority_queue(const Compare cmp); // additional constructor with parameter compare function
+	// we are OK with the compile-generated
+	priority_queue(const priority_queue&)            = default;
+	priority_queue& operator=(const priority_queue&) = default;
+	~priority_queue()                                = default;
+
+private :
+	/* helpers */
+	/// Return left child of this index
+	size_t get_left_child_ind(size_t index) const;
+	/// Return right child of this index
+	size_t get_right_child_ind(size_t index) const;
+	/// Return parent of this index, -1 if the element has no parent
+	int get_parent_ind(size_t index) const;
+	/// Sift element at index down
+	void sift_down(size_t index);
+	/// Sift element at index up
+	void sift_up(size_t index);
+
+public:
+	/* interface */
+	/// Access top element, but don't change it. O(1) time complexity
+	const T& top() const;
+	/// Insert element O(logn) time complexity
+	void push(const T& el);
+	/// Remove top element O(logn) time complexity
+	void pop();
+	/// Clear content
+	void clear();
+	/// Test whether container is empty
+	bool empty() const;
+	/// Return size
+	size_t size() const;
+};
+
+template<class T, class Compare>
+priority_queue<T, Compare>::priority_queue(const Compare cmp) : cmp(cmp) {
+	//...
+}
+
+template<class T, class Compare>
+inline
+size_t priority_queue<T, Compare>::get_left_child_ind(size_t index) const {
+	
+	return index * 2 + 1;
+}
+
+template<class T, class Compare>
+inline
+size_t priority_queue<T, Compare>::get_right_child_ind(size_t index) const {
+	
+	return index * 2 + 2;
+}
+
+template<class T, class Compare>
+inline
+int priority_queue<T, Compare>::get_parent_ind(size_t index) const {
+	
+	return (index) ? (index - 1) / 2 : -1 ;
+}
+
+template<class T, class Compare>
+void  priority_queue<T, Compare>::sift_down(size_t index) {
+	
+	if (empty())
+		return;
+
+	T el_to_swap = data[index];
+	size_t left_child_ind = get_left_child_ind(index);
+
+	size_t size = data.size();
+	// sift until wanted position is reached
+	while (left_child_ind < size) {
+		int right_child_ind = get_right_child_ind(index);
+		// with which child to swap
+		if (left_child_ind < size - 1 && cmp(data[left_child_ind] , data[right_child_ind]))
+			left_child_ind = right_child_ind;
+		// the element is on its position
+		if (!cmp(el_to_swap, data[left_child_ind]))
+			break;
+		// go down one level more 
+		data[index] = data[left_child_ind];
+		index = left_child_ind;
+		left_child_ind = get_left_child_ind(left_child_ind);
+	}
+
+	data[index] = el_to_swap;
+}
+
+template<class T, class Compare>
+void  priority_queue<T, Compare>::sift_up(size_t index) {
+	
+	T el_to_swap = data[index];
+	int cur_parent = get_parent_ind(index);
+
+	while (cur_parent >= 0 && cmp(data[cur_parent], el_to_swap))
+	{
+		data[index] = data[cur_parent];
+		index = cur_parent;
+		cur_parent = get_parent_ind(index);
+	}
+	data[index] = el_to_swap;
+}
+
+template<class T, class Compare>
+const T& priority_queue<T, Compare>::top() const {
+	
+	return data.front();
+}
+
+template<class T, class Compare>
+void priority_queue<T, Compare>::push(const T& el) {
+	
+	data.push_back(el);
+	sift_up(data.size() - 1);
+}
+
+template<class T, class Compare>
+void  priority_queue<T, Compare>::pop() {
+	
+	std::swap(data.front(), data.back());
+	data.pop_back();
+	sift_down(0);
+}
+
+template<class T, class Compare>
+void  priority_queue<T, Compare>::clear() {
+	
+	data.clear();
+}
+
+template<class T, class Compare>
+inline bool priority_queue<T, Compare>::empty() const {
+	
+	return data.empty();
+}
+
+template<class T, class Compare>
+inline size_t priority_queue<T, Compare>::size() const {
+	
+	return data.size();
+}
+} // namespace dsa
