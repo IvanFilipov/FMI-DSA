36dc931a1437ffb39ac5b2eab674b5a84f775722
IvanFilipov <vanaka1189@gmail.com>
2019-03-04 16:37:27 +0200

    bs_tree: merge tests.cpp/h into example.cpp

--- data_structures/tree/binary_search_tree/template_bst/example.cpp
+++ data_structures/tree/binary_search_tree/template_bst/example.cpp
@@ -6,11 +6,58 @@
  * @file   main.cpp
  * @author Ivan Filipov
  * @date   02.2019
- * @brief  Main for running tests for our custom binary search tree.
+ * @brief  Main for example usage
+ *         and running some tests 
+ *         for our custom binary search tree.
  */
 
-#include <iostream>
-#include "tests.h"
+#include <iostream> // std::cout
+
+#include "../../../../utils/student.h"
+#include "bs_tree.hpp"
+
+/** Run a fixed tree tests */
+void run_basic_tests() {
+	// create empty tree
+	dsa::bs_tree<dsa::student> tree;
+	// add some elements
+	tree.insert({ "Ivancho", 40000 });
+	tree.insert({ "Mariika", 25000 });
+	tree.insert({ "Gencho", 50001 });
+	tree.insert({ "Pencho", 25000 });
+	tree.insert({ "Genka", 42000 });
+	tree.insert({ "Penka", 25000 });
+	tree.insert({ "Kalin", 40000 });
+	tree.insert({ "Kalinka", 25000 });
+
+	std::cout << "\ntree's height = " << tree.get_height() << std::endl;
+	tree.print_sorted_keys(std::cout);
+	
+	std::cout << "\n\nsearching for Ivancho :" << std::endl;
+	try {
+		const dsa::student& s = tree.find({ "Ivancho", 40000 });
+		std::cout << "found! His fn is : " << s.fn << std::endl;
+	} catch (...) {
+		std::cout << "not found!" << std::endl;
+	}
+	
+	std::cout << "\n\nsearching for Ivan :" << std::endl;
+	try {
+		const dsa::student& s = tree.find({ "Ivan", 200 });
+		std::cout << "found! His fn is : " << s.fn << std::endl;
+	} catch (...) {
+		std::cout << "not found!" << std::endl;
+	}
+
+	std::cout << "\nremoving \"Genka\"...";
+	try {
+		tree.remove({ "Genka", 42000 });
+		std::cout << "done!" << std::endl;
+	} catch(...) {}
+	tree.print_sorted_keys(std::cout);
+	std::cout << std::endl;
+}
+
 
 int main() {
 	
