2bd3068b040ebedc74062dcb357f899b09a67f08
IvanFilipov <vanaka1189@gmail.com>
2019-04-09 23:04:48 +0300

    list_sorting: fix comments

--- algorithms/sorting_and_searching/list_sorting/example.cpp
+++ algorithms/sorting_and_searching/list_sorting/example.cpp
@@ -1,10 +1,23 @@
-#include <iostream>
-#include <cstdlib> //rand(), srand()
-#include <ctime> //time()
+/*******************************************************************************
+ * This file is part of the "Data structures and algorithms" course. FMI 2018/19 
+ *******************************************************************************/
+
+/**
+ * @file   example.cpp
+ * @author Ivan Filipov
+ * @date   12.2018
+ * @brief  Example usage and test of merge sort and
+ *         insertion sort on singly linked list.
+ */
+
+#include <iostream> // std::cout
+#include <cstdlib>  // std::rand(), std::srand()
+#include <ctime>    // std::time()
 
 #include "../../../utils/student.h"
 #include "minimal_list.hpp"   // minimal slightly linked list
 
+/// test merge sort on fixed linked list
 void test_custom_merge_sort() {
 	
 	std::cout << "MERGE sort testing" << std::endl
@@ -15,7 +28,7 @@ void test_custom_merge_sort() {
 	dsa::minimal_list<int> list;
 	
 	for (int i = 0; i < 10; i++)
-		list.push_back(rand() % 100);
+		list.push_back(std::rand() % 100);
 
 	std::cout << "starting list items : \n";
 	list.print_elems(std::cout);
@@ -36,6 +49,7 @@ void test_custom_merge_sort() {
 	std::cout << std::endl << std::endl;
 }
 
+/// test insertion sort on fixed linked list
 void test_custom_insertion_sort() {
 	
 	std::cout << "Insertion sort testing" << std::endl
@@ -46,7 +60,7 @@ void test_custom_insertion_sort() {
 	dsa::minimal_list<int> list;
 	
 	for (int i = 0; i < 10; i++)
-		list.push_back(rand() % 100);
+		list.push_back(std::rand() % 100);
 
 	std::cout << "starting list items : \n";
 	list.print_elems(std::cout);
--- algorithms/sorting_and_searching/list_sorting/minimal_list.hpp
+++ algorithms/sorting_and_searching/list_sorting/minimal_list.hpp
@@ -1,88 +1,122 @@
-/*
- * A minimal implementation of singly linked list, support for Merge sort and Insertion sort.
- * This file is part of the "Data structures and algorithms" course. FMI 2018/19
- *
- * Author : Ivan Filipov	
+/*******************************************************************************
+ * This file is part of the "Data structures and algorithms" course. FMI 2018/19 
+ *******************************************************************************/
+
+/**
+ * @file   minimal_list.hpp
+ * @author Ivan Filipov
+ * @date   12.2018
+ * @brief  A minimal implementation of singly linked list,
+ *         with support for Merge sort and Insertion sort.
  */
 
 #pragma once
  
 #include <stdexcept>        // exception types
-#include <iosfwd>           // forward declarations of ostreams
+#include <ostream>          // std::ostream
 #include <initializer_list> // initializer_list
 
 namespace dsa {
+/**
+ * @class minimal_list
+ * @brief A singly-linked list with minimal interface.
+ *
+ * @tparam T: Type of stored elements.
+ */
 template<typename T>
 class minimal_list {
-
-private :
-	// an inner reres_ptrentation of each "box" 
+private:
+	/**
+	 *  @struct node
+	 *  @brief  An inner representation of each "box".
+	 */ 
 	struct node {
-
-		T data;         // the actual stored data
-		node* next_ptr; // a pointer to the next box
-		
-		// a simple constructor
+		T     data;     //!< the actual stored data
+		node* next_ptr; //!< a pointer to the next box
+		/** Creates an node by given data. */
 		node(const T& ndata, node* next_ptr = nullptr) :
 			data(ndata), next_ptr(next_ptr) { /**/ }
 	};
 
-private :
 	/* private data members */
-	node* front_ptr; // pointer to the first box
-	node* back_ptr;  // pointer to the last box 
-	size_t cur_size; // current amount of objects stored
+	node*  front_ptr; //!< pointer to the first box
+	node*  back_ptr;  //!< pointer to the last box 
+	size_t cur_size;  //!< current amount of objects stored
 
 public:
 	/* object life cycle */
+	/** Creates %minimal_list with no elements */
 	minimal_list();
+	
+	/** Creates %minimal_list from initializer list */
 	minimal_list(std::initializer_list<T> il);
-	// not copyable
+	
+	/** minimal_list is not copyable */
 	minimal_list(const minimal_list& rhs) = delete;
+	
+	/** minimal_list is not copyable */
 	minimal_list& operator=(const minimal_list& rhs) = delete;
+	
+	/** Frees all memory allocated. */
 	~minimal_list();
-
-	//help functions
+	
 private:
 	/* helpers */
-	// frees the memory allocated for all list boxes
+	/** frees the memory allocated for all list boxes */
 	void clear();
-	//takes two sorted parts and returns their sorted union
+	
+	/**
+	 * @brief merge sort helper: takes two sorted parts and returns their sorted union
+	 * @param[in] first_ptr: pointer to the first part
+	 * @param[in] second_ptr: pointer to the second part
+	 * @retval pointer to the merged result
+	 */
 	node* merge(node* first_ptr, node* second_ptr);
-	// merge sort algorithm
-	// completely independent of the list implementation
-	// needs to know only where the list beginning is
+	
+	/**
+	 * @brief Merge sort algorithm
+	 * @param[in] head_ptr: pointer to the beginning of the list to be sorted.
+	 * @param[in] size: amount of objects in the list
+	 * @retval pointer to the sorted list
+	 *
+	 * Completely independent of the list implementation
+	 * needs to know only where the list beginning is
+	 */
 	node* merge_sort(node* head_ptr, size_t size);
 	
 public:
 	/* interface */
 	// modifiers
-	// adds element at the end O(1)
+	/** adds element at the end O(1) */
 	void push_back(const T& el);
-	// adds element at the beginning O(1)
+	
+	/** adds element at the end O(1) */
 	void push_front(const T& el); 
-	// sorting using merge
+	
+	/** Sorts the list using merge sort */
 	void sort();
-	// sorting using selection sort
+	
+	/** Sorts the list using selection sort */
 	void slow_sort();
-	//simply outputs the contain of the list
+	
+	/** simply outputs the contain of the list */
 	void print_elems(std::ostream& os) const;
-	// checks if list is empty :
+	
+	/** Check if the list is empty */
 	bool empty() const { return cur_size == 0; }
 };
 
 
 template<typename T>
-minimal_list<T>::minimal_list() : 
+minimal_list<T>::minimal_list(): 
 	front_ptr(nullptr), back_ptr(nullptr), cur_size(0) {
-
 	//...
 }
 
 template<typename T>
-minimal_list<T>::minimal_list(std::initializer_list<T> il) : minimal_list() {
+minimal_list<T>::minimal_list(std::initializer_list<T> il): minimal_list() {
 	
-	for (const T& el : il)
+	for (const T& el: il)
 		push_back(el);
 }
 
@@ -203,7 +237,6 @@ typename minimal_list<T>::node* minimal_list<T>::
 	// while there are elements in both list
 	// get the bigger one, add it to the result
 	while (first_ptr && second_ptr) {
-
 		if (first_ptr->data < second_ptr->data) {
 			head_ptr->next_ptr = first_ptr;
 			first_ptr = first_ptr->next_ptr;
