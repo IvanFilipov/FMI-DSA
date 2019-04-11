fe01af0dce625ca65038892d17eeef2cee3becb9
IvanFilipov <vanaka1189@gmail.com>
2019-04-11 22:44:01 +0300

    abstract_sorting: fixing style & comments

--- algorithms/sorting_and_searching/abstract_sorting/abstract_sorting.hpp
+++ algorithms/sorting_and_searching/abstract_sorting/abstract_sorting.hpp
@@ -1,25 +1,37 @@
-/*
- * An example with abstract implementation of some sorting algorithms.
- * They depend only on iterators and comparators, no matter what the data structure below is.
- * The idea of this implementation is taken from https://en.cppreference.com
- * This file is part of the "Data structures and algorithms" course. FMI 2018/19
+/*******************************************************************************
+ * This file is part of the "Data structures and algorithms" course. FMI 2018/19 
+ *******************************************************************************/
+
+/**
+ * @file   abstract_sorting.hpp
+ * @author Ivan Filipov
+ * @date   12.2019
+ * @brief  An example with abstract implementation of some sorting algorithms.
  *
- * Author : Ivan Filipov	
+ * They depend only on iterators and comparators, no matter what the data structure below is.
+ * 
+ * @note The idea of this implementation is taken from @see https://en.cppreference.com
  */
 
 #pragma once
 
-#include <algorithm>  // std::swap, std::rotate, std::upper_bound
-                      // std::inplace_merge, std::make_heap, std::sort_heap
+#include <algorithm>  // std::swap(), std::rotate(), std::upper_bound()
+                      // std::inplace_merge(), std::make_heap(), std::sort_heap()
  
 namespace dsa {
-// abstract implementation of selection sort algorithm
-// uses random access iterator
+/**
+ * @brief STL like Insertion sort
+ * @param[in] being: iterator to the beginning of the set
+ * @param[in] iterator to the end of the set
+ * @param[in] cmp: comparator to be used
+ * @tparam Iterator: type of iterator - should provide random access 
+ * @tparam Comparator: type of comparator
+ *
+ * for std::rotate @see https://en.cppreference.com/w/cpp/algorithm/rotate
+ * for std::upper_bound @see https://en.cppreference.com/w/cpp/algorithm/upper_bound
+ */
 template <typename Iterator, typename Comparator>
 void insertion_sort(Iterator begin, Iterator end, Comparator cmp) {
-	
-	// for std::rotate see : https://en.cppreference.com/w/cpp/algorithm/rotate
-	// for std::upper_bound see : https://en.cppreference.com/w/cpp/algorithm/upper_bound
 	
 	for (Iterator i = begin; i != end; ++i) {
         std::rotate(std::upper_bound(begin, i, *i, cmp),
@@ -27,11 +39,19 @@ void insertion_sort(Iterator begin, Iterator end, Comparator cmp) {
     }
 }
 
-// abstract implementation of merge sort algorithm
+/**
+ * @brief STL like Merge sort
+ * @param[in] being: iterator to the beginning of the set
+ * @param[in] iterator to the end of the set
+ * @param[in] cmp: comparator to be used
+ * @tparam Iterator: type of iterator - should be at least bidirectional
+ * @tparam Comparator: type of comparator
+ *
+ * for std::inplace_merge @see https://en.cppreference.com/w/cpp/algorithm/inplace_merge
+ */
 template <typename Iterator, typename Comparator>
 void merge_sort(Iterator begin, Iterator end, Comparator cmp) {
-	
-	// for std::inplace_merge see : https://en.cppreference.com/w/cpp/algorithm/inplace_merge
+
     if (end - begin > 1) {
         Iterator middle = begin + (end - begin) / 2;
         merge_sort(begin, middle, cmp);
@@ -40,9 +60,20 @@ void merge_sort(Iterator begin, Iterator end, Comparator cmp) {
     }
 }
 
+/**
+ * @brief STL like Heap sort
+ * @param[in] being: iterator to the beginning of the set
+ * @param[in] iterator to the end of the set
+ * @param[in] cmp: comparator to be used
+ * @tparam Iterator: type of iterator - should provide random access 
+ * @tparam Comparator: type of comparator
+ *
+ * for std::make_heap @see https://en.cppreference.com/w/cpp/algorithm/make_heap
+ * for std::sort_heap @see https://en.cppreference.com/w/cpp/algorithm/sort_heap
+ */
 template <typename Iterator, typename Comparator>
 void heap_sort(Iterator begin, Iterator end, Comparator cmp) {
-	// find those algorithms at  https://en.cppreference.com/w/cpp/algorithm
+
 	std::make_heap(begin, end, cmp);
     std::sort_heap(begin, end, cmp);
 }
