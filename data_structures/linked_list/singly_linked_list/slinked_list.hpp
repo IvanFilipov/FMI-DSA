78ac5e9eb442c2006618c31b8349f58b00e83510
IvanFilipov <vanaka1189@gmail.com>
2019-04-14 18:01:36 +0300

    singly_linked_list: fix comments & style

--- data_structures/linked_list/singly_linked_list/slinked_list.hpp
+++ data_structures/linked_list/singly_linked_list/slinked_list.hpp
@@ -1,60 +1,86 @@
-/*
- * A basic linear data structure - singly linked list. With forward iterator iterator. STL friendly.
- * This file is part of the "Data structures and algorithms" course. FMI 2018/19
- *
- * Author : Ivan Filipov	
+/*******************************************************************************
+ * This file is part of the "Data structures and algorithms" course. FMI 2018/19 
+ *******************************************************************************/
+
+/**
+ * @file   linked_list.hpp
+ * @author Ivan Filipov
+ * @date   10.2018
+ * @brief  A basic linear data structure - singly linked list.
+ *         With forward iterator iterator. STL friendly.
+ * @see https://en.wikipedia.org/wiki/Linked_list
  */
 
 #pragma once
  
 #include <stdexcept>        // exception types
-#include <iosfwd>           // forward declarations of ostreams
-#include <initializer_list> // initializer_list
-#include <iterator>         // declaration of forward_iterator_tag
+#include <ostream>          // std::ostream
+#include <initializer_list> // std::initializer_list
+#include <iterator>         // declaration of bidirectional_iterator_tag
 
 namespace dsa {
 template<typename T>
+/**
+ * @class slinked_list
+ * @brief Basic container for elements. 
+ *        Supports fast insertion/removal at position pointed by an iterator.
+ *        Fast random access is not supported.
+ *        Only forward iterations are possible.
+ * @tparam T: type of elements stored
+ */
 class slinked_list {
-
-private :
-	// an inner representation of each "box" 
+private:
+	/**
+	 *  @struct node
+	 *  @brief  An inner representation of each "box".
+	 */ 
 	struct node {
-
-		T data;         // the actual stored data
-		node* next_ptr; // a pointer to the next box
-		
-		// a simple constructor
-		node(const T& ndata, node* pnext = nullptr) :
+		T data;         //!< the actual stored data
+		node* next_ptr; //!< a pointer to the next box
+		/** Creates an node by given data. */
+		node(const T& ndata, node* pnext = nullptr):
 			data(ndata), next_ptr(pnext) { /**/ }
 	};
 
-private :
+private:
 	/* private data members */
-	node* front_ptr; // pointer to the first box
-	node* back_ptr;  // pointer to the last box 
-	size_t cur_size; // current amount of objects stored
-
-public :
-	/* object life cycle */
-	slinked_list();
-	slinked_list(std::initializer_list<T> il);
-	slinked_list(const slinked_list& rhs);
-	slinked_list& operator=(const slinked_list& rhs);
-	~slinked_list();
+	node* front_ptr; //!< pointer to the first box
+	node* back_ptr;  //!< pointer to the last box 
+	size_t cur_size; //!< current amount of objects stored
 
 public:
+	/* object life cycle */
+	/** Creates %slinked_list with no elements */
+	slinked_list();
 	
-	typedef T value_type; // for std::back_inserter to work
+	/** Creates %slinked_list from initializer list */
+	slinked_list(std::initializer_list<T> il);
 	
+	/**
+	  *  @brief     %slinked_list copy constructor.
+	  *  @param[in] rhs: A %slinked_list of identical element type, from which to copy.
+	  */
+	slinked_list(const slinked_list& rhs);
+	
+	/**
+	  *  @brief     %slinked_list assignment operator.
+	  *  @param[in] rhs: A %slinked_list of identical element type, from which to copy.
+	  */
+	slinked_list& operator=(const slinked_list& rhs);
+	
+	/** Frees all memory allocated. */
+	~slinked_list();
+	
+public:
 	/* iterator */
-	// very basic forward iterator
+	typedef T value_type; //!< type of stored elements
+	
+	/// very basic forward iterator
 	class iterator {
 		// only the container will have access to the private data of iterators
 		friend class slinked_list<T>;
-		
 	private:
-
-		node* node_ptr;
+		node* node_ptr; //!< pointer to a box from %slinked_list
 
 	public:
 		// lets make our iterator more STL friendly 
@@ -65,22 +91,26 @@ public:
 		typedef T*                        pointer;
 		typedef std::forward_iterator_tag iterator_category;
 		typedef ptrdiff_t                 difference_type;
-		// basic constructor
-		iterator(node* n) : node_ptr(n) { }
+		/** Basic iterator constructor - points to a box from the list */
+		iterator(node* n): node_ptr(n) {}
 		
 	public:
-		
-		// access operators
+		//@{
+		/** access operators */
 		reference operator*() { return node_ptr->data; }
 		const reference operator*() const { return node_ptr->data; } // for const iterators
 		pointer operator->() { return &node_ptr->data; }
+		//@}
 		
-		// compare operators
+		//@{
+		/** compare operators */
 		bool operator==(const iterator& rhs) const { return node_ptr == rhs.node_ptr; }
 		bool operator!=(const iterator& rhs) const { return !(rhs == *this); }
 		explicit operator bool() const { return node_ptr != nullptr; }
+		//@}
 		
-		// modifier operators
+		//@{
+		/** modifier operators */
 		self_type& operator++() {
 			
 			node_ptr = node_ptr->next_ptr;
@@ -93,90 +123,158 @@ public:
 			++(*this);
 			return res;
 		}
+		//@}
 	};
 	
 private:
 	/* helpers */
-	// copy all objects from another list
+	/** 
+	  *  @brief     copies all elements for another list
+	  *  @param[in] rhs: list from which to copy.
+	  */
 	void copy_from(const slinked_list& rhs);
 	
 public:
 	/* interface */
-	// frees the memory allocated for all list boxes
-	void clear();
-	
-	// modifiers :
-	// adds element at the end O(1)
-	void push_back(const T& el);
-	// adds element at the beginning O(1)
-	void push_front(const T& el); 
-	// lookup for the last element O(1)
-	const T& back() const;
-	T& back();
-	// lookup for the first element O(1)
-	const T& front() const;
+	/**
+	 * @brief  get the first element from the list
+	 * @retval Reference to the front element.
+	 * @throw  std::logic_error if the list is empty
+	 * Time complexity O(1).
+	 */
 	T& front();
-	// remove the last element O(n)
-	void pop_back();
-	// remove the first element O(1)
+	
+	/**
+	 * @brief  get the first element from the list
+	 * @retval Read-only reference to the front element.
+	 * @throw  std::logic_error if the list is empty
+	 * Time complexity O(1).
+	 */
+	const T& front() const;
+	
+	/**
+	 * @brief  get the last element from the list
+	 * @retval Reference to the back element.
+	 * @throw  std::logic_error if the list is empty
+	 * Time complexity O(1).
+	 */
+	T& back();
+	
+	/**
+	 * @brief  get the last element from the list
+	 * @retval Read-only reference to the back element.
+	 * @throw  std::logic_error if the list is empty
+	 * Time complexity O(1).
+	 */
+	const T& back() const;
+	
+	/**
+	 * @brief         Push a new element in the beginning of the list.
+	 * @param[in] el: Value to be inserted
+	 * Time complexity O(1).
+	 */
+	void push_front(const T& el); 
+	
+	/**
+	 * @brief         Push a new element at the end of the list.
+	 * @param[in] el: Value to be inserted
+	 * Time complexity O(1).
+	 */
+	void push_back(const T& el);
+	
+	/** Remove the first element. Time complexity O(1). */
 	void pop_front();
-	// inserts a new element, after the element pointed by iterator O(1)
+	
+	/** Remove the last element. Time complexity O(1). */
+	void pop_back();
+	
+	/**
+	 * @brief         Insert a new element after an iterator to a random position in the list.
+	 * @param[in] it: Iterator to the position after which to insert the new element.
+	 * @param[in] el: Value to be inserted
+	 * Time complexity O(1).
+	 */
 	iterator insert_after(const iterator& it, const T& el);
-	// removes the element after the element pointed by iterator O(1)
+	
+	/**
+	 * @brief         Remove an element after an iterator to a random position in the list.
+	 * @param[in] it: Iterator to the position after which to remove the element.
+	 * @param[in] el: Value to be inserted
+	 * Time complexity O(1).
+	 */
 	iterator remove_after(const iterator& it);
 	
-	// getters :
-	bool empty() const;
-	size_t size() const;
-
-	//simply outputs the contain of the list
-	void print_elems(std::ostream& os) const;
-	//searches the list for a given element, if found
-	//returns an iterator to it, else iterator : end(), O(n)
+	/**
+	 * @brief         Searches the list for a specific element.
+	 * @param[in] el: Element to be searched for.
+	 * @retval    iterator to the element if found
+	 * @retval    %end iterator, if there is no such element
+	 * Time complexity O(n) - linear in elements count.
+	 */
 	iterator find(const T& el) const;
 	
-	// iterators creation interface
-	iterator begin() {
-
-		return iterator(front_ptr);
-	}
-
-	iterator end() {
-
-		return iterator(nullptr);
-	}
+	/** Get iterator to the beginning of the list */
+	iterator begin() { return iterator(front_ptr); }
+	
+	/** Get iterator to the end of the list */
+	iterator end() { return iterator(nullptr); }
 	
 	// constant iterator objects. they have limited usage
 	// in our implementation, much better is to create
 	// another class : const_iterator
+	/** Get constant iterator to the beginning of the list */
 	const iterator cbegin() const {
 
 		return iterator(front_ptr);
 	}
-
+	
+	/** Get constant iterator to the end of the list */
 	const iterator cend() const {
 
 		return iterator(nullptr);
 	}
+	
+	/**
+	 * @brief  Checks if the %slinked_list is empty.
+	 * @retval boolean: whether the slinked_list is empty.
+	 * Time complexity O(1).
+	 */
+	bool empty() const;
+	
+	/**
+	 * @brief Get the current size of the list.
+ 	 * @retval Current size.
+ 	 * Time complexity O(1).
+	 */
+	size_t size() const;
+	
+	/** Frees the resources for the list. Time complexity liner in the count of the elements. */
+	void clear();
+	
+	/**
+	 *  @brief Debug print of the list.
+	 *  @param[in,out] os: a stream to write to.
+	 */
+	void print(std::ostream& os) const;
 };
 
-/* object life cycle */
 
 template<typename T>
-slinked_list<T>::slinked_list() : 
+slinked_list<T>::slinked_list(): 
 	front_ptr(nullptr), back_ptr(nullptr), cur_size(0) {
 
 	//...
 }
+
 template<typename T>
-slinked_list<T>::slinked_list(std::initializer_list<T> il) : slinked_list() {
+slinked_list<T>::slinked_list(std::initializer_list<T> il): slinked_list() {
 	
-	for (const T& el : il)
+	for (const T& el: il)
 		push_back(el);
 }
 
 template<typename T>
-slinked_list<T>::slinked_list(const slinked_list<T>& rhs) : 
+slinked_list<T>::slinked_list(const slinked_list<T>& rhs): 
 	front_ptr(nullptr), back_ptr(nullptr), cur_size(0) {
 
 	copy_from(rhs);
@@ -199,13 +297,10 @@ slinked_list<T>::~slinked_list() {
 	clear();
 }
 
-/* helpers */
-
 template<typename T>
 void slinked_list<T>::copy_from(const slinked_list<T>& rhs) {
 
-	if (rhs.empty())
-		return;
+	if (rhs.empty()) return;
 
 	try {
 		//copy head
@@ -230,27 +325,6 @@ void slinked_list<T>::copy_from(const slinked_list<T>& rhs) {
 	}
 	// equal size at the end, if everything is alright
 	cur_size = rhs.cur_size;
-}
-
-/* interface */
-
-template<typename T>
-void slinked_list<T>::clear() {
-
-	node* destroyer;
-	// iterating through the elements
-	while (front_ptr != nullptr) {
-		// remember where the current 'head' is
-		destroyer = front_ptr;
-		// move one step forward
-		front_ptr = front_ptr->next_ptr;
-		// remove the 'old head'
-		delete destroyer;
-	}
-	// zeros all members
-	front_ptr = nullptr;
-	back_ptr = nullptr;
-	cur_size = 0;
 }
 
 template<typename T>
@@ -299,7 +373,7 @@ template<typename T>
 const T& slinked_list<T>::front() const {
 
 	if (empty())
-		throw std::logic_error("the list is empty!");
+		throw std::logic_error("the list is empty");
 
 	return front_ptr->data;
 }
@@ -316,7 +390,7 @@ template<typename T>
 const T& slinked_list<T>::back() const {
 
 	if (empty())
-		throw std::logic_error("the list is empty!");
+		throw std::logic_error("the list is empty");
 		
 	return back_ptr->data;
 }
@@ -325,7 +399,7 @@ template<typename T>
 void slinked_list<T>::pop_front() {
 
 	if (empty())
-		throw std::logic_error("the list is empty!");
+		throw std::logic_error("the list is empty");
 	
 	if (cur_size == 1) {
 		// remove the first and zero the structure
@@ -344,7 +418,7 @@ template<typename T>
 void slinked_list<T>::pop_back() {
 
 	if (empty())
-		throw std::logic_error("the list is empty!");
+		throw std::logic_error("the list is empty");
 	
 	if (cur_size == 1) {
 		clear();
@@ -383,7 +457,7 @@ template<typename T>
 typename slinked_list<T>::iterator slinked_list<T>::
 	insert_after(const typename slinked_list<T>::iterator& it, const T& el) {
 	
-	//insert after the last element
+	// insert after the last element
 	if (it == end() || it.node_ptr == back_ptr) {
 		push_back(el);
 		return iterator(back_ptr);
@@ -398,6 +472,7 @@ typename slinked_list<T>::iterator slinked_list<T>::
 	++cur_size; 
 	return new_node;
 }
+
 template<typename T>
 typename slinked_list<T>::iterator slinked_list<T>::
 	remove_after(const typename slinked_list<T>::iterator& it) {
@@ -427,9 +502,40 @@ typename slinked_list<T>::iterator slinked_list<T>::
 }
 
 template<typename T>
-void slinked_list<T>::print_elems(std::ostream& os) const {
+inline
+bool slinked_list<T>::empty() const {
 
-	os << "content : ";
+	return cur_size == 0;
+}
+
+template<typename T>
+inline
+size_t slinked_list<T>::size() const {
+
+	return cur_size;
+}
+
+template<typename T>
+void slinked_list<T>::clear() {
+
+	node* destroyer;
+	// iterating through the elements
+	while (front_ptr != nullptr) {
+		// remember where the current 'head' is
+		destroyer = front_ptr;
+		// move one step forward
+		front_ptr = front_ptr->next_ptr;
+		// remove the 'old head'
+		delete destroyer;
+	}
+	// zeros all members
+	front_ptr = nullptr;
+	back_ptr = nullptr;
+	cur_size = 0;
+}
+
+template<typename T>
+void slinked_list<T>::print(std::ostream& os) const {
 	
 	if(cur_size == 0) {
 		os << "{}" << std::endl;	
@@ -448,20 +554,5 @@ void slinked_list<T>::print_elems(std::ostream& os) const {
 		it = it->next_ptr;
 	}
 	os << it->data << " }" << std::endl;
-}
-
-template<typename T>
-inline
-bool slinked_list<T>::empty() const {
-
-	return cur_size == 0;
-}
-
-
-template<typename T>
-inline
-size_t slinked_list<T>::size() const {
-
-	return cur_size;
 }
 } // namespace dsa
